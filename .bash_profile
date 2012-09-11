a62a02655a4f2b34ca7061bc6ae37e71633a9c34
Morgan Wigmanich <okize123@gmail.com>
2012-09-11 14:56:42 -0400

    initial commit of dotfiles

---
+++ .bash_profile
@@ -0,0 +1,24 @@
+# path settings
+HOMEBREW=/usr/local/bin:/usr/local/sbin
+NODE=/usr/local/lib/node:/usr/local/lib/node_modules
+GIT=/usr/local/git/bin
+RBENV=~/.rbenv/shims:/usr/bin/gcc-4.2
+#PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
+export PATH=$HOMEBREW:$NODE:$GIT:$RBENV:$PATH
+
+# Load ~/.extra, ~/.bash_prompt, ~/.exports, ~/.aliases and ~/.functions
+# ~/.extra used for settings I don’t want to commit
+for file in ~/.{extra,bash_prompt,exports,aliases,functions}; do
+	[ -r "$file" ] && source "$file"
+done
+unset file
+
+
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+#[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
+
+# init z   https://github.com/rupa/z
+#. ~/code/z/z.sh
+
+# init rbenv
+eval "$(rbenv init -)"
---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfilesBackup           # old dotfiles backup directory
+files=".aliases .bash_profile .bash_prompt .bashrc .exports .extra .functions .gitconfig .gitignore .gitattributes .inputrc .osx"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfilesBackup in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfilesBackup directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir & Creating symlink to $files in home directory."
+for file in $files; do
+    mv ~/$file $olddir
+    ln -s $dir/$file ~/$file
+done
