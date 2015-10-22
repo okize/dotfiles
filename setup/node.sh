f551ffbb7f4fa7a5c19ec1a61f395412f01aa727
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-10-22 14:24:45 -0400

    update npm installs

--- setup/node.sh
+++ setup/node.sh
@@ -6,18 +6,20 @@ nvm use stable
 nvm alias default node
 
 # Development
-npm install -g babel-eslint
-npm install -g browser-repl
+npm install -g bower
 npm install -g browserify
 npm install -g coffee-script
+npm install -g coffeelint
 npm install -g ember-cli
+npm install -g eslint_d
 npm install -g grunt-cli
 npm install -g gulp
+npm install -g mocha
 npm install -g nodemon
+npm install -g webpack
 
 # Tools
-npm install -g bower
-npm install -g mocha
+npm install -g browser-repl
 npm install -g stylestats
 
 # My modules
