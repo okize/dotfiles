f450fe77383761ccc3f730016efcf99f55f19b5a
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2018-09-06 06:56:45 -0400

    don't bother installing a bunch of npm globals

--- setup/node.sh
+++ setup/node.sh
@@ -6,19 +6,4 @@ nvm use stable
 nvm alias default node
 
 # Development
-npm install -g express-generator
-npm install -g grunt-cli
-npm install -g gulp
-npm install -g iectrl
-npm install -g node-inspector
-npm install -g nodemon
-
-# Tools
-npm install -g browser-repl
-npm install -g stylestats
-
-# My modules
-npm install -g iconr
-npm install -g npm-popular
-npm install -g selector-detector
-npm install -g trendie
+# npm install -g iectrl # IE VMs
