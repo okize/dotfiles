eb7425f660ef9038053b1f0fbd9b097a9997fff2
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2019-05-09 07:00:00 -0400

    update node setup script

--- setup/node.sh
+++ setup/node.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# Install most recent stable node & set default node version for shell
-nvm install stable
-nvm use stable
+NODE_VERSION=`cat ~/.nvmrc`
+echo "installing NodeJS $NODE_VERSION..."
+
+# Install node version specified in .nvmrc
+nvm install $NODE_VERSION
+# Set default node version for shell
+nvm use $NODE_VERSION
 nvm alias default node
 
 # Development
 # npm install -g iectrl # IE VMs
+npm install serverless -g
