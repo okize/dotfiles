f8a4e8872d3d728d0344a1de0e6a06b1837a03bc
Morgan Wigmanich <mwigmanich@wistia.com>
2019-06-06 07:00:00 -0400

    accept xcode license

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,6 +54,9 @@ echo "Installing binaries and apps with Homebrew"
 brew bundle --verbose
 echo "...done"
 
+# Accept Xcode license
+sudo xcodebuild -license
+
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
