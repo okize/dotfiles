428167abac65fcb8c11eb2ce35b7e143aecd63ae
Morgan Wigmanich <mwigmanich@wistia.com>
2019-06-07 07:00:00 -0400

    install the vs code sync extension

--- bootstrap.sh
+++ bootstrap.sh
@@ -94,5 +94,12 @@ echo "Installing fonts"
 rsync --exclude ".DS_Store" -av --no-perms fonts/ ~/Library/Fonts/
 echo "...done"
 
+echo "Finalizing..."
+# install VS Code settings sync extension
+code --install-extension Shan.code-settings-sync
+
 # symlink diff-highlight into path
 sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/bin/diff-highlight
+
+echo "$(tput bold)Done. Note that some of these changes require a logout/restart to take effect.$(tput sgr 0)"
+echo "To sync VS Code settings, open Code and paste Github Personal Access Token & Gist ID
