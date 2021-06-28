a304418471fcf756038c175ed21f633edb242d57
Morgan Wigmanich <mwigmanich@wistia.com>
2021-06-28 19:03:15 -0400

    add explanatory comment

--- setup/keymap.sh
+++ setup/keymap.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# https://dchakarov.com/blog/macbook-remap-keys/
+# remap caps lock key to ctrl
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
