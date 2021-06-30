c26091737a0e6e5c24af99a4ea272042a3249a30
Morgan Wigmanich <mwigmanich@wistia.com>
2021-06-30 18:06:05 -0400

    update node version

--- setup/node.sh
+++ setup/node.sh
@@ -5,12 +5,11 @@ echo "installing NodeJS $NODE_VERSION..."
 
 # Install node version specified in .nvmrc
 nvm install $NODE_VERSION
+
 # Set default node version for shell
 nvm use $NODE_VERSION
 nvm alias default node
 
 # Development
-# npm install -g iectrl # IE VMs
-npm install serverless -g
-npm install create-react-app
-npm install expo-cli
+npm install create-react-app -g
+npm install expo-cli -g
