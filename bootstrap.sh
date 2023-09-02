b56390d4c2ee99b9c4fc906ae74f629fdc46beb1
Morgan Wigmanich <okize123@gmail.com>
2023-09-02 08:00:00 -0400

    refactor bootstrap script to use a series of function calls

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-set -eo pipefail
 
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 # as well as assorted other "setup" tasks
 ############################
 
+# order of operations
+main() {
+  symlink_dotfiles
+  set_computer_name
+  install_xcode_cli
+  install_homebrew
+  install_brewfile_packages
+  setup_macos
+}
+
 # dotfiles directory
 dir=~/dotfiles
 
 # old dotfiles backup directory
 olddir=~/dotfilesBackup
+
+# pre-emptive sudo signin
+sudo echo ""
 
 function log_section() {
   echo "$(tput setaf 0)$(tput setab 7)$(tput bold) $1 $(tput sgr 0)"
@@ -20,99 +32,111 @@ function log_step() {
   echo "$(tput setaf 6)- $1$(tput sgr 0)"
 }
 
-# create dotfilesBackup in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+function symlink_dotfiles() {
+  # create dotfilesBackup in homedir
+  echo "Creating $olddir for backup of any existing dotfiles in ~"
+  mkdir -p $olddir
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+  # change to the dotfiles directory
+  echo "Changing to the $dir directory"
+  cd $dir
 
-# list of files/folders to symlink in homedir
-files=".aliases .bash_profile .bash_prompt .bashrc .exports .functions .gitconfig .gitignore .inputrc .gemrc .irbrc .pryrc .secrets .tool-versions .wgetrc"
+  # list of files/folders to symlink in homedir
+  files=".aliases .bash_profile .bash_prompt .bashrc .exports .functions .gitconfig .gitignore .inputrc .gemrc .irbrc .pryrc .secrets .tool-versions .wgetrc"
 
-# move any existing dotfiles in homedir to dotfilesBackup directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-echo "Moving any existing dotfiles from ~ to $olddir & Creating symlink to $files in home directory."
-for file in $files; do
-    mv ~/$file $olddir
-    ln -s $dir/$file ~/$file
-    echo "symlinking $dir/$file -> ~/$file"
-done
+  # move any existing dotfiles in homedir to dotfilesBackup directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+  echo "Moving any existing dotfiles from ~ to $olddir & Creating symlink to $files in home directory."
+  for file in $files; do
+      mv ~/$file $olddir
+      ln -s $dir/$file ~/$file
+      echo "symlinking $dir/$file -> ~/$file"
+  done
 
-# pre-emptive sudo signin
-sudo echo ""
+  # reload shell session
+  source ~/.bashrc;
+}
 
 # optionally set computer name
-echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  echo "What would you like it to be?"
-  read COMPUTER_NAME
-  sudo scutil --set ComputerName $COMPUTER_NAME
-  sudo scutil --set HostName $COMPUTER_NAME
-  sudo scutil --set LocalHostName $COMPUTER_NAME
-  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
-fi
+function set_computer_name() {
+  echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
+  read -r response
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "What would you like it to be?"
+    read COMPUTER_NAME
+    sudo scutil --set ComputerName $COMPUTER_NAME
+    sudo scutil --set HostName $COMPUTER_NAME
+    sudo scutil --set LocalHostName $COMPUTER_NAME
+    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+  else
+    echo "Skipping computer rename"
+  fi
+}
 
-# install Xcode Command Line Tools if not installed.
-if xcode-select -p > /dev/null; then
-  log "Xcode Command Line Tools already installed"
-else
-  xcode-select --install
-fi
+# install Xcode Command Line Tools if not installed
+install_xcode_cli() {
+  if xcode-select -p > /dev/null; then
+    echo "XCode CLI tools already installed"
+  else
+    xcode-select --install
+  fi
+}
 
 # check for Homebrew & install if necessary
-if ! [ -x "$(command -v brew)" ]; then
-  log "Homebrew not found, installing it"
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  UNAME_MACHINE="$(/usr/bin/uname -m)"
-  if [[ "${UNAME_MACHINE}" == "arm64" ]]
-  then
-    HOMEBREW_PREFIX="/opt/homebrew"
-  else
-    HOMEBREW_PREFIX="/usr/local"
+install_homebrew() {
+  if ! [ -x "$(command -v brew)" ]; then
+    echo "Homebrew not found, installing it"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-fi
+}
 
-# setup homebrew and apps
-echo "Installing binaries and apps with Homebrew"
-brew bundle --verbose
-echo "...done"
+# install brews & casks from Brewfile
+install_brewfile_packages() {
+  echo "Installing binaries and apps with Homebrew"
+  brew bundle --verbose
 
-# check for any problems with homebrew
-brew bundle check
-brew doctor
+  # map vi so it opens the brew-installed vim
+  ln -s $HOMEBREW_PREFIX/bin/vim $HOMEBREW_PREFIX/bin/vi
 
-# map vi so it opens the brew-installed vim
-ln -s /usr/local/bin/vim /usr/local/bin/vi
+  # check for any problems with homebrew
+  brew bundle check
+  brew doctor
+}
 
-# setup macOS
-echo "Setting up macOS"
-source ./setup/macos.sh
-echo "...done"
+# install brews & casks from Brewfile
+install_brewfile_packages() {
+  echo "Installing binaries and apps with Homebrew"
+  brew bundle --verbose
 
-# install fonts
-echo "Installing fonts"
-rsync --exclude ".DS_Store" -av --no-perms fonts/ ~/Library/Fonts/
-echo "...done"
+  # map vi so it opens the brew-installed vim
+  ln -s /usr/local/bin/vim /usr/local/bin/vi
 
-echo "Finalizing..."
+  # check for any problems with homebrew
+  brew bundle check
+  brew doctor
+}
 
-# symlink diff-highlight into path
-sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/bin/diff-highlight
+setup_macos() {
+  echo "Setting up macOS"
+  source ./setup/macos.sh
 
-# remap keyboard keys
-# source ./setup/keymap.sh
+  echo "Installing fonts"
+  rsync --exclude ".DS_Store" -av --no-perms fonts/ ~/Library/Fonts/
 
-# persist keymap after system reboot
-sudo defaults write com.apple.loginwindow LoginHook ~/dotfiles/setup/keymap.sh
+  # symlink diff-highlight into path
+  sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/bin/diff-highlight
 
-# attempt to add iOS Simulator to the dock
-# for some reason this can't be in setup/macos.sh
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-killall Dock
+  # remap keyboard keys
+  # source ./setup/keymap.sh
+
+  # persist keymap after system reboot
+  sudo defaults write com.apple.loginwindow LoginHook ~/dotfiles/setup/keymap.sh
+
+  # attempt to add iOS Simulator to the dock
+  # for some reason this can't be in setup/macos.sh
+  defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+  killall Dock
+}
+
+main "$@"
 
 echo "\n$(tput bold)Done. Note that some of these changes require a logout/restart to take effect.$(tput sgr 0)"
